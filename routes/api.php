13c22e10394aa57872b0ef40ad72657fd92cb95b
kimtrietvan <kimtrietvan@gmail.com>
2023-10-17 00:10:49 +0700

    V0.1, API WORKING

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PinterestController;
+use App\Http\Middleware\ValidateCreatePin;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/user/{sess}", function (string $sess) {
+    return PinterestController::get_user_data_from_cookie($sess);
+});
+
+Route::get("/user/{sess}/board/", function (string $sess) {
+    $user_data = PinterestController::get_user_data_from_cookie($sess);
+    return PinterestController::get_board_list($sess, $user_data["username"]);
+});
+
+Route::post("/user/board/create", function (Request $request) {
+    $data = array();
+    $sess = $request->sess;
+    $data["board_id"] = $request->board_id;
+    $data["image_url"] = $request->image_url ?? "";
+    $data["video_url"] = $request->video_url ?? "";
+    $data["title"] = $request->title ?? "";
+    $data["description"] = $request->description ?? "";
+    $data["link"] = $request->link ?? "";
+    $data["alt_text"] = $request->alt_text ?? "";
+    return PinterestController::post_pin_to_board($sess, $data);
+
+
+})->middleware(ValidateCreatePin::class);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PinterestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    print_r(PinterestController::get_board_list( 'TWc9PSZyZ3FZbDZTMnJRWnhWRlZlZ1hMaVZMTjdvcEo3YmMrMGVVTC9OYkpnYUVnUTFpWmFvTEZmZk1YWUpOQmpZOERuZVVoNmMwaGhwcTdVNC83ZFRkVHg1a1JOOFViQ2JvNHZnaDJnb3RTc2NJcW90V3BVb1FOcnZETUdoVW1PWW9YVHRCSEhPRVpmY1pYQnhNZHhoc2pKR1lTWmFRSHBQazlZaWliY2VzbUNlRHV1SXJva0lJZ3dEcXlGM29rVnRjcm54UDVnT1lPMUdvbjJrODVVZ1B3ZkpkeE1lN24wTUJFeVdadENXZkJwamx4bW5TbnJ4b2pUWjV4YmcwNWFCV2FUaW9yVUlRQWxWalFFUmFua1Z2MXV2VUFkNk9SelJZTW5FcktTOHNjL3dYeHZvUGpEMjJOMlZYY2VaMGdsbWlKdUdzN1NpUm8wQTY0WFo0RDE5RnluWS9ES0JiZVFQSEo4cmJrMGZUZnFiMURiV3h6L2lmNFlnWSswSWgydHkvcE5Rc1VjbXlIQVNyeVhJemg0VStLM2NRPT0mR3lrNGdWQ1BvN09GTVFualFFc3F5Zmp0dlowPQ==', 'kimtrietvan'));
+});
+
+Route::get('/test1', function () {
+    $data = array();
+    $data["board_id"] = "1067353249126508421";
+    $data["title"] = "Test";
+    $data["note"] = "test";
+    $data["image_url"] = "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
+
+    print_r(PinterestController::post_pin_to_board("TWc9PSZyZ3FZbDZTMnJRWnhWRlZlZ1hMaVZMTjdvcEo3YmMrMGVVTC9OYkpnYUVnUTFpWmFvTEZmZk1YWUpOQmpZOERuZVVoNmMwaGhwcTdVNC83ZFRkVHg1a1JOOFViQ2JvNHZnaDJnb3RTc2NJcW90V3BVb1FOcnZETUdoVW1PWW9YVHRCSEhPRVpmY1pYQnhNZHhoc2pKR1lTWmFRSHBQazlZaWliY2VzbUNlRHV1SXJva0lJZ3dEcXlGM29rVnRjcm54UDVnT1lPMUdvbjJrODVVZ1B3ZkpkeE1lN24wTUJFeVdadENXZkJwamx4bW5TbnJ4b2pUWjV4YmcwNWFCV2FUaW9yVUlRQWxWalFFUmFua1Z2MXV2VUFkNk9SelJZTW5FcktTOHNjL3dYeHZvUGpEMjJOMlZYY2VaMGdsbWlKdUdzN1NpUm8wQTY0WFo0RDE5RnluWS9ES0JiZVFQSEo4cmJrMGZUZnFiMURiV3h6L2lmNFlnWSswSWgydHkvcE5Rc1VjbXlIQVNyeVhJemg0VStLM2NRPT0mR3lrNGdWQ1BvN09GTVFualFFc3F5Zmp0dlowPQ==", $data));
 });
