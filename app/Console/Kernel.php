73d8654f8fa22f8e718b5c3c6c2738acd7ff3dc0
kimtrietvan <kimtrietvan@gmail.com>
2023-11-24 12:52:20 +0700

    Product sync add

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,14 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
+use App\Models\PinterestAccount;
+use App\Models\PinterestBoard;
+use App\Http\Controllers\PinterestController;
+use App\Models\Woocommerce;
+use App\Models\WoocommerceProduct;
+use App\Models\WoocommerceProductImage;
+use App\Models\WoocommerceProductSync;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +21,103 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+//            PinterestBoard::query()->delete();
+            $accounts = PinterestAccount::all();
+            foreach ($accounts as $account) {
+                $cookie = $account['cookie'];
+                $username = $account['username'];
+                $proxy = $account['proxy'] ?? '';
+                $response = PinterestController::get_board_list($cookie, $username, $proxy);
+                if ($response->status() == 200) {
+                    $user_data = $response->json();
+                    if ($user_data['resource_response']['status'] == 'success' && $user_data['resource_response']['message'] == 'ok') {
+                        $board_list = $user_data['resource_response']['data'];
+                        foreach (PinterestBoard::where('pinterest_id', '=', $account['id'])->get() as $pinterest_db) {
+                            if (in_array($pinterest_db['board_id'], array_column($board_list, 'id'))) {
+                                continue;
+                            }
+                            else {
+                                PinterestBoard::where('board_id', $pinterest_db['board_id'])->delete();
+                            }
+                        }
+                        foreach ($board_list as $board) {
+                            if (PinterestBoard::where('board_id', '=', $board['id'])->first() !== null) {
+                                $been_update_board = PinterestBoard::where('board_id', '=', $board['id'])->first();
+                                if ($been_update_board->board_name != $board['name']) {
+                                    $been_update_board->board_name = $board['name'];
+                                    $been_update_board->save();
+                                }
+                                continue;
+                            }
+                            PinterestBoard::create([
+                                'pinterest_id' => $account['id'],
+                                'board_id' => $board['id'],
+                                'board_name' => $board['name']
+                            ]);
+                        }
+                    }
+                }
+
+            }
+        })->everyThirtySeconds();
+
+
+        $schedule->call(function () {
+
+            $sync_request = WoocommerceProductSync::where('finished', 0)->get();
+                    foreach ($sync_request as $request) {
+                        $site = Woocommerce::where('id', $request->woocommerce_id)->first();
+                        // return print_r($site);
+                        $base_url = $site->base_url;
+                        $cs = $site->cs;
+                        $ck = $site->ck;
+                        $page = 1;
+                        $remote_products = [];
+                        do {
+                        $response = Http::withHeaders([
+                            'Authorization' => 'Basic '. base64_encode($ck.":".$cs)
+                        ])->get($base_url . "/wp-json/wc/v3/products?per_page=100&page=".$page)->json();
+                        if (count($response) > 0)
+                        $remote_products = array_merge($remote_products, $response);
+                        $page++;
+                        } while (count($response) > 0);
+        
+                        if (WoocommerceProduct::where('woocommerce_id', $site->id)->first()) {
+                            $products = [];
+                            foreach (WoocommerceProduct::where('woocommerce_id', $site->id)->get() as $product) {
+                                array_push($products, $product);
+                            }
+            
+                            $local_products_id = array_column($products, 'product_code');
+                            $remote_products_id = array_column($remote_products, 'id');
+                            $delete_products = array_diff($local_products_id, $remote_products_id);
+                            foreach ($delete_products as $delete_product) {
+                                $product = WoocommerceProduct::where('product_id', $delete_product)->where(['woocommerce_id', $site->id])->first();
+                                if ($product) $product->delete();
+                            }
+                        }
+                        foreach ($remote_products as $product) {
+                            $product_result = WoocommerceProduct::where("product_code", $product['id'])->where('woocommerce_id', $site->id)->first();
+                            if (!$product_result) {
+                                $new_product = new WoocommerceProduct;
+                                $new_product->product_code = $product['id'];
+                                $new_product->product_name = $product['name'];
+                                $new_product->product_url = $product['permalink'];
+                                $new_product->woocommerce_id = $site->id;
+                                $new_product->save();
+                                foreach ($product['images'] as $image) {
+                                    $images = new WoocommerceProductImage;
+                                    $images->product_image = $image['src'];
+                                    $images->product_id = $new_product->id;
+                                    $images->save();
+                                }
+                            }
+                            }
+                        $request->finished = 1;
+                        $request->save();
+                    }
+        })->everySecond();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\WoocommerceProduct;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PinterestController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Models\Woocommerce;
+use App\Models\WoocommerceProductImage;
+use App\Models\WoocommerceProductSync;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,20 +20,132 @@ use App\Http\Controllers\PinterestController;
 |
 */
 
+
+Route::get("/login", function () {
+    if (Auth::check()) {
+        return redirect(route('dashboard'));
+    }
+    return view('login');
+})->name('login');
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/register', function () {
+    if (Auth::check()) {
+        return redirect(route('dashboard'));
+    }
+    return view('register');
+})->name('register');
+
+Route::post('/register', [LoginController::class, 'register']);
+
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        return redirect(route('dashboard'));
+    }
+    return redirect(\route('login'));
 });
 
+Route::get('/dashboard', function () {
+    return view('user_dashboard');
+})->middleware('auth')->name('dashboard');
+
+Route::match(['get', 'post'], '/logout', function () {
+    Auth::logout();
+    return redirect()->route('login');
+});
+
+Route::get('/dashboard/pinterest/add', function () {
+    return view('add_pinterest_account');
+})->middleware('auth')->name('pinterest_add_account');
+
+Route::post('/dashboard/pinterest/add', [DashboardController::class, 'add_account_pinterest']);
+Route::post('/dashboard/pinterest/check', [DashboardController::class, 'check_account_pinterest'])->name('check_account_pinterest');
+
+Route::prefix('/dashboard')->middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('user_dashboard');
+    })->middleware('auth')->name('dashboard');
+    Route::post('/pinterest/add', [DashboardController::class, 'add_account_pinterest']);
+    Route::post('/pinterest/check', [DashboardController::class, 'check_account_pinterest'])->name('check_account_pinterest');
+
+    Route::get('/pinterest/manager', function () {
+        return view('dashboard_pinterest_manager');
+    })->middleware('auth')->name("pinterest_account_manager");
+    Route::post('/pinterest/delete', [DashboardController::class, 'delete_account_pinterest']);
+    Route::post('/quick_shot', [\App\Http\Controllers\QuickShotController::class, 'index']);
+
+    Route::get('/woocommerce', function () {
+        $id = Auth::id();
+        $woocommerce_data = Woocommerce::where('user_id', $id)->get();
+        return view('dashboard_woocommerce', ['data' => $woocommerce_data]);
+    })->name('woocommerce_manager');
+    Route::get('/woocommerce/add', function () {
+        return view('dashboard_woocommerce_add');
+    })->name('woocommerce_add_site');
+
+    Route::post('/woocommerce/add', [\App\Http\Controllers\WoocommerceManagerController::class, 'add']);
+    Route::post('/woocommerce/delete', [\App\Http\Controllers\WoocommerceManagerController::class, 'delete'])->name('woocommerce_delete_site');
+    Route::get('/woocommerce/products', function () {
+        $woocommerce_data = Woocommerce::where("user_id", Auth::id())->get();
+        return view('woocommerce_products', ['data' => $woocommerce_data]);
+    })->name('woocommerce_product');
+    Route::post('/woocommerce/sync', [\App\Http\Controllers\WoocommerceManagerController::class,'syncSite'])->name('woocommerce_sync_site');
+});
 Route::get('/test', function () {
-    print_r(PinterestController::get_board_list( 'TWc9PSZyZ3FZbDZTMnJRWnhWRlZlZ1hMaVZMTjdvcEo3YmMrMGVVTC9OYkpnYUVnUTFpWmFvTEZmZk1YWUpOQmpZOERuZVVoNmMwaGhwcTdVNC83ZFRkVHg1a1JOOFViQ2JvNHZnaDJnb3RTc2NJcW90V3BVb1FOcnZETUdoVW1PWW9YVHRCSEhPRVpmY1pYQnhNZHhoc2pKR1lTWmFRSHBQazlZaWliY2VzbUNlRHV1SXJva0lJZ3dEcXlGM29rVnRjcm54UDVnT1lPMUdvbjJrODVVZ1B3ZkpkeE1lN24wTUJFeVdadENXZkJwamx4bW5TbnJ4b2pUWjV4YmcwNWFCV2FUaW9yVUlRQWxWalFFUmFua1Z2MXV2VUFkNk9SelJZTW5FcktTOHNjL3dYeHZvUGpEMjJOMlZYY2VaMGdsbWlKdUdzN1NpUm8wQTY0WFo0RDE5RnluWS9ES0JiZVFQSEo4cmJrMGZUZnFiMURiV3h6L2lmNFlnWSswSWgydHkvcE5Rc1VjbXlIQVNyeVhJemg0VStLM2NRPT0mR3lrNGdWQ1BvN09GTVFualFFc3F5Zmp0dlowPQ==', 'kimtrietvan'));
+
+    $sync_request = WoocommerceProductSync::where('finished', 0)->get();
+            foreach ($sync_request as $request) {
+                $site = Woocommerce::where('id', $request->woocommerce_id)->first();
+                // return print_r($site);
+                $base_url = $site->base_url;
+                $cs = $site->cs;
+                $ck = $site->ck;
+                $page = 1;
+                $remote_products = [];
+                do {
+                $response = Http::withHeaders([
+                    'Authorization' => 'Basic '. base64_encode($ck.":".$cs)
+                ])->get($base_url . "/wp-json/wc/v3/products?per_page=100&page=".$page)->json();
+                if (count($response) > 0)
+                $remote_products = array_merge($remote_products, $response);
+                $page++;
+                } while (count($response) > 0);
+
+                if (WoocommerceProduct::where('woocommerce_id', $site->id)->first()) {
+                    $products = [];
+                    foreach (WoocommerceProduct::where('woocommerce_id', $site->id)->get() as $product) {
+                        array_push($products, $product);
+                    }
+    
+                    $local_products_id = array_column($products, 'product_code');
+                    $remote_products_id = array_column($remote_products, 'id');
+                    $delete_products = array_diff($local_products_id, $remote_products_id);
+                    foreach ($delete_products as $delete_product) {
+                        $product = WoocommerceProduct::where(['product_id', $delete_product], ['woocommerce_id', $site->id])->first();
+                        if ($product) $product->delete();
+                    }
+                }
+                foreach ($remote_products as $product) {
+                    $product_result = WoocommerceProduct::where(["product_code", $product['id']], ['woocommerce_id', $site->id])->first();
+                    if (!$product_result) {
+                        $new_product = new WoocommerceProduct;
+                        $new_product->product_code = $product['id'];
+                        $new_product->product_name = $product['name'];
+                        $new_product->product_url = $product['permalink'];
+                        $new_product->woocommerce_id = $site->id;
+                        $new_product->save();
+                        foreach ($product['images'] as $image) {
+                            $images = new WoocommerceProductImage;
+                            $images->product_image = $image['src'];
+                            $images->product_id = $new_product->id;
+                            $images->save();
+                        }
+                    }
+                    }
+                $request->finished = 1;
+                $request->save();
+            }
 });
 
-Route::get('/test1', function () {
-    $data = array();
-    $data["board_id"] = "1067353249126508421";
-    $data["title"] = "Test";
-    $data["note"] = "test";
-    $data["image_url"] = "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
 
-    print_r(PinterestController::post_pin_to_board("TWc9PSZyZ3FZbDZTMnJRWnhWRlZlZ1hMaVZMTjdvcEo3YmMrMGVVTC9OYkpnYUVnUTFpWmFvTEZmZk1YWUpOQmpZOERuZVVoNmMwaGhwcTdVNC83ZFRkVHg1a1JOOFViQ2JvNHZnaDJnb3RTc2NJcW90V3BVb1FOcnZETUdoVW1PWW9YVHRCSEhPRVpmY1pYQnhNZHhoc2pKR1lTWmFRSHBQazlZaWliY2VzbUNlRHV1SXJva0lJZ3dEcXlGM29rVnRjcm54UDVnT1lPMUdvbjJrODVVZ1B3ZkpkeE1lN24wTUJFeVdadENXZkJwamx4bW5TbnJ4b2pUWjV4YmcwNWFCV2FUaW9yVUlRQWxWalFFUmFua1Z2MXV2VUFkNk9SelJZTW5FcktTOHNjL3dYeHZvUGpEMjJOMlZYY2VaMGdsbWlKdUdzN1NpUm8wQTY0WFo0RDE5RnluWS9ES0JiZVFQSEo4cmJrMGZUZnFiMURiV3h6L2lmNFlnWSswSWgydHkvcE5Rc1VjbXlIQVNyeVhJemg0VStLM2NRPT0mR3lrNGdWQ1BvN09GTVFualFFc3F5Zmp0dlowPQ==", $data));
-});
